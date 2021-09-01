825f4f7fb27c7468c7653b4732893a31fdf8d339
HARSHA N <60893840+Harshan1999@users.noreply.github.com>
2021-09-01 12:45:51 +0530

    Update SavingsCalculator.java

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -31,7 +31,7 @@ public class SavingsCalculator {
     private static int remainingDaysInMonth(LocalDate date){
         YearMonth yearMonth=YearMonth.of(date.getYear(), date.getMonth());
         int totalDaysInMonth= yearMonth.lengthOfMonth();
-        int remainingDays= date.getDayOfMonth();
+        int remainingDays= totalDaysInMonth-date.getDayOfMonth();
         return remainingDays;
     }
     public float calculate(){
